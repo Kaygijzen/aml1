d691912515ce1bb67a445c2c94d28f8384ab367d
Kay Gijzen <58943291+Kaygijzen@users.noreply.github.com>
2023-10-11 21:29:19 +0200

    BostonTest Random/Grid Search and SMBO

--- main.py
+++ main.py
@@ -18,6 +18,10 @@ warnings.filterwarnings("ignore")
 
 
 class BostonTest():
+    data = sklearn.datasets.fetch_openml(data_id=43465)
+    X_train, X_valid, y_train, y_valid = sklearn.model_selection.train_test_split(
+        data.data, data.target, test_size=0.33, random_state=1)
+    
     def __init__(self) -> None:
         self.cs = ConfigSpace.ConfigurationSpace('boston.cs', 1)
 
@@ -26,6 +30,14 @@ class BostonTest():
         gamma = ConfigSpace.UniformFloatHyperparameter(
             name='gamma', lower=3.0517578125e-05, upper=8, log=True, default_value=0.1)
         self.cs.add_hyperparameters([C, gamma])
+
+        self.theta_inc_performance = 0
+    
+    def optimizee(self, gamma, C):
+        clf = sklearn.svm.SVR()
+        clf.set_params(kernel='rbf', gamma=gamma, C=C)
+        clf.fit(self.X_train, self.y_train)
+        return clf.score(self.X_valid, self.y_valid)
 
     def grid_cs(self, n_configurations):
         n = int(round(np.sqrt(n_configurations)))
@@ -41,111 +53,99 @@ class BostonTest():
             for configuration in self.cs.sample_configuration(n_configurations)
         ])
     
-
-    def test_smbo():
-        pass
-        # return perf array
-
     def test_grid(self, n_configurations):
-        grid = self.grid_cs(n_configurations)
+        configurations = self.grid_cs(n_configurations)
+        n = len(configurations)
+        perf = np.zeros(n)
+
+        theta_inc_performance = 0
+
+        for i, theta_new in enumerate(configurations):
+            print(f"grid search iteration {i+1}/{len(configurations)}")
+            performance = self.optimizee(theta_new["gamma"], theta_new["C"])
+            theta_inc_performance = max(theta_inc_performance, performance)
+            print(f"perf {theta_inc_performance}")
+            print('----------------------------')
+            perf[i] = theta_inc_performance
+
+        return perf
+
+    def test_random(self, n_configurations):
+        svr = sklearn.svm.SVR()
+        distributions = self.cs.get_hyperparameters_dict()
+        clf = sklearn.model_selection.RandomizedSearchCV(svr, distributions, n_iter=n_configurations)
+        search = clf.fit(self.X_train, self.y_train)
+        return search.score(self.X_valid, self.y_valid)
+
+    def test_smbo(self, n_configurations):
+        np.random.seed(0)
+
+        data = sklearn.datasets.fetch_openml(data_id=43465)
+        X_train, X_valid, y_train, y_valid = sklearn.model_selection.train_test_split(
+            data.data, data.target, test_size=0.33, random_state=1)
+
+        def sample_configurations(n_configurations):
+            # function uses the ConfigSpace package, as developed at Freiburg University.
+            # most of this functionality can also be achieved by the scipy package
+            # same hyperparameter configuration as in scikit-learn
+            cs = ConfigSpace.ConfigurationSpace('sklearn.svm.SVR', 1)
+
+            C = ConfigSpace.UniformFloatHyperparameter(
+                name='C', lower=0.03125, upper=32768, log=True, default_value=1.0)
+            gamma = ConfigSpace.UniformFloatHyperparameter(
+                name='gamma', lower=3.0517578125e-05, upper=8, log=True, default_value=0.1)
+            cs.add_hyperparameters([C, gamma])
+
+            return np.array([(configuration['gamma'],
+                                configuration['C'])
+                            for configuration in cs.sample_configuration(n_configurations)])
+
+        def sample_initial_configurations(n: int) -> typing.List[typing.Tuple[np.array, float]]:
+            configs = sample_configurations(n)
+            return [((gamma, C), self.optimizee(gamma, C)) for gamma, C in configs]
 
 
-        for config in grid:
-            print(config["C"], config["gamma"])
-            break
-        # return perf array
+        smbo = SequentialModelBasedOptimization()
+        smbo.initialize(sample_initial_configurations(16))
 
-    def test_random():
-        pass
-        # return perf array
+        n = 64
+        perf = np.zeros(n)
 
+        for idx in range(n):
+            print('iteration %d/64' % idx)
+            smbo.fit_model()
+            theta_new = smbo.select_configuration(sample_configurations(1024))
+            performance = self.optimizee(theta_new[0], theta_new[1])
+            # plt.plot(performance)
+            # plt.show()
+            print(performance)
+            smbo.update_runs((theta_new, performance))
+            print(smbo.theta_inc_performance)
+            print('----------------------------')
+            perf[idx] = smbo.theta_inc_performance
 
-    def run():
-        # run all test
-        # and plot perf's
-        pass
+        return perf
 
+    def run(self, n):
+        # Grid search
+        grid_search_performance = self.test_grid(n)
 
+        # Random search
+        random_search_performance = self.test_random(n)
+        
+        #SMBO
+        smbo_performance = self.test_smbo(n)
 
-def svm_boston_smbo():
-    np.random.seed(1)
+        # Plotting performance
+        plt.plot(grid_search_performance, label="Grid Search")
+        plt.axhline(y = random_search_performance, color="r", label="Random Search") 
+        plt.plot(smbo_performance, label="SMBO")
+        plt.legend()
+        plt.show()
 
-    data = sklearn.datasets.fetch_openml(data_id=43465)
-    X_train, X_valid, y_train, y_valid = sklearn.model_selection.train_test_split(
-        data.data, data.target, test_size=0.33, random_state=1)
-
-    def optimizee(gamma, C):
-        clf = sklearn.svm.SVR()
-        clf.set_params(kernel='rbf', gamma=gamma, C=C)
-        clf.fit(X_train, y_train)
-        return clf.score(X_valid, y_valid)#sklearn.metrics.accuracy_score(y_valid, clf.predict(X_valid))
-
-
-    def sample_configurations(n_configurations):
-        # function uses the ConfigSpace package, as developed at Freiburg University.
-        # most of this functionality can also be achieved by the scipy package
-        # same hyperparameter configuration as in scikit-learn
-        cs = ConfigSpace.ConfigurationSpace('sklearn.svm.SVR', 1)
-
-        C = ConfigSpace.UniformFloatHyperparameter(
-            name='C', lower=0.03125, upper=32768, log=True, default_value=1.0)
-        gamma = ConfigSpace.UniformFloatHyperparameter(
-            name='gamma', lower=3.0517578125e-05, upper=8, log=True, default_value=0.1)
-        cs.add_hyperparameters([C, gamma])
-
-        return np.array([(configuration['gamma'],
-                            configuration['C'])
-                        for configuration in cs.sample_configuration(n_configurations)])
-
-    def sample_initial_configurations(n: int) -> typing.List[typing.Tuple[np.array, float]]:
-        configs = sample_configurations(n)
-        return [((gamma, C), optimizee(gamma, C)) for gamma, C in configs]
-
-
-    smbo = SequentialModelBasedOptimization()
-    smbo.initialize(sample_initial_configurations(16))
-
-    print(smbo.theta_inc_performance)
-    print(smbo.theta_inc_performance)
-    print('starting loop')
-
-    n = 256
-    perf = np.zeros(n)
-
-    for idx in range(n):
-        print('iteration %d/64' % idx)
-        smbo.fit_model()
-        theta_new = smbo.select_configuration(sample_configurations(1024))
-        performance = optimizee(theta_new[0], theta_new[1])
-        # plt.plot(performance)
-        # plt.show()
-        print(performance)
-        smbo.update_runs((theta_new, performance))
-        print(smbo.theta_inc_performance)
-        print('----------------------------')
-        perf[idx] = smbo.theta_inc_performance
-
-    return perf
-
-def svm_boston_grid():
-    pass
-
-def svm_boston_random():
-    pass
 
 def main():
-    perf = svm_boston_smbo()
-
-    # # print(perf)
-
-    plt.plot(perf)
-    plt.show()
-
-    # boston = BostonTest()
-    # grid = boston.grid(16)
-    # print(len(grid))
-
-    # boston.test_grid(10)
+    BostonTest().run(64)
 
 
 if __name__ == '__main__':
