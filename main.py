65a4a52192e2635052507acaaa4909717528f9b2
jelle-westra <jelwestra@gmail.com>
2023-10-26 16:57:22 +0200

    grid plot

--- main.py
+++ main.py
@@ -52,18 +52,17 @@ class ModelTest():
     def sample_cs(self, n_configs):
         return self.cs.sample_configuration(n_configs)
     
-
     def test_grid(self, n_configs):
-        configs = self.grid_cs(n_configs)
+        self.configs_grid = self.grid_cs(n_configs)
         perf = np.zeros(n_configs)
-        for i, config in tqdm(enumerate(configs), total=n_configs):
+        for i, config in tqdm(enumerate(self.configs_grid), total=n_configs):
             perf[i] = max(perf[i-1], self.optimizee(config))
         return perf
     
     def test_rand(self, n_configs):
-        configs = self.sample_cs(n_configs)
+        self.configs_rand = self.sample_cs(n_configs)
         perf = np.zeros(n_configs)
-        for i, config in tqdm(enumerate(configs), total=n_configs):
+        for i, config in tqdm(enumerate(self.configs_rand), total=n_configs):
             perf[i] = max(perf[i-1], self.optimizee(config))
         return perf
     
@@ -94,15 +93,46 @@ class ModelTest():
             smbo.update_runs((config, performance))
             perf[idx+n_configs_init] = smbo.theta_inc_performance
 
-        return perf
+        return perf, smbo
+    
+    def test_smbo_normalized(self, n_iter, n_configs_init=3, n_configs_loop=128):
+        def sample_configs(n_configs):
+            return np.array([
+                [value for _, value in config.items()]
+                for config in self.sample_cs(n_configs)
+            ])
 
-    def run(self, n_configs):
+        def sample_init_configs(configs):
+            return [(
+                np.array([value for _, value in config.items()]),
+                self.optimizee(config)
+            ) for config in configs]        
+        
+        smbo = SequentialModelBasedOptimization()
+
+        sample = self.sample_cs(n_configs_init)
+
+        smbo.initialize(sample_init_configs(sample))
+
+        perf = np.zeros(n_iter).astype(float)
+        perf[:n_configs_init] = np.nan
+
+        for idx in trange(n_iter - n_configs_init):
+            smbo.fit_model()
+            config = smbo.select_configuration(sample_configs(n_configs_loop))
+            performance = self.optimizee({hyp : val for hyp, val in zip(self.hyperparams, config)})
+            smbo.update_runs((config, performance))
+            perf[idx+n_configs_init] = smbo.theta_inc_performance
+
+        return perf, smbo
+
+    def run(self, n_configs, smbo_n_init_configs, smbo_n_samples):
         print('grid search')
         self.grid_perf = self.test_grid(n_configs)
         print('random search')
         self.rand_perf = self.test_rand(n_configs)
         print('smbo search')
-        self.smbo_perf = self.test_smbo(n_configs)
+        self.smbo_perf, self.smbo = self.test_smbo(n_configs, smbo_n_init_configs, smbo_n_samples)
        
     def show(self):
         plt.plot(self.grid_perf, label="Grid Search")
@@ -110,6 +140,8 @@ class ModelTest():
         plt.plot(self.smbo_perf, label="SMBO")
         plt.legend()
         plt.show()
+
+    
         
 
 def svr_test(datasets, n_configs, smbo_n_init_configs=8, smbo_n_samples=128):
@@ -130,8 +162,38 @@ def svr_test(datasets, n_configs, smbo_n_init_configs=8, smbo_n_samples=128):
             random_state=1
         )
 
-        test.run(n_configs=n_configs)
-        test.show()
+        test.run(n_configs, smbo_n_init_configs, smbo_n_samples)
+
+
+        fig, ax = plt.subplots(1, 2, figsize=(10,5))
+
+
+        ax[0].plot(test.grid_perf, label="Grid Search")
+        ax[0].plot(test.rand_perf, label="Random Search")
+        ax[0].plot(test.smbo_perf, label="SMBO")
+        ax[0].set_xlabel('iterations')
+        ax[0].set_ylabel('score')
+        ax[0].legend(frameon=False)
+    
+        C_vals, gamma_vals = zip(*[(c['C'],c['gamma']) for c in test.configs_grid])
+        ax[1].loglog(C_vals, gamma_vals, 'C0.', ms=10)
+        grid_best = test.configs_grid[test.grid_perf.argmax()]
+        ax[1].plot(grid_best['C'], grid_best['gamma'], 'C0.', ms=20)
+
+        C_vals, gamma_vals = zip(*[(c['C'],c['gamma']) for c in test.configs_rand])
+        ax[1].plot(C_vals, gamma_vals, 'C1.', ms=10, alpha=.2)
+        rand_best = test.configs_rand[test.rand_perf.argmax()]
+        ax[1].plot(rand_best['C'], rand_best['gamma'], 'C1.', ms=20)
+        
+        # TODO smbo_n_itit configs toevoegen
+        sbmo_configs = np.vstack([c for c, _ in test.smbo.capital_r])[4:]
+        ax[1].plot(*sbmo_configs.T, 'C2.', ms=10)
+        ax[1].plot(*sbmo_configs[np.argmax(test.smbo_perf)].T, 'C2.', ms=20) 
+        ax[1].set_xlabel('C')
+        ax[1].set_ylabel('gamma')
+
+        fig.tight_layout()
+        plt.show()
 
 
 def random_forest_test(datasets, n_configs):
@@ -163,11 +225,23 @@ def random_forest_test(datasets, n_configs):
             random_state=0
         )
 
-        test.run(n_configs=n_configs)
-        test.show()
+        # test.run(n_configs=n_configs)
+        # test.show()
+
 
 
 def main():
+    plt.rcParams['axes.spines.top'] = False
+    plt.rcParams['axes.spines.right'] = False
+    plt.rcParams['xtick.direction'] = 'in'
+    plt.rcParams['ytick.direction'] = 'in'
+
+    plt.rc('font', family='serif', serif="cmr10", size=16)
+    plt.rc('mathtext', fontset='cm', rm='serif')
+    plt.rc('axes', unicode_minus=False)
+
+    plt.rcParams['axes.formatter.use_mathtext'] = True
+
     # datasets : https://www.openml.org/search?type=data&status=active
     datasets = {
         'boston-housing' : 43465,
@@ -178,7 +252,7 @@ def main():
     # TODO : (optional) sommige datasets hebben pre-processing nodig van string bv naar one-hot encoding,
     #        misschien gewoon makkelijke datasets kiezen die dit niet hebben om het zo makkelijk mogelijk te maken
 
-    svr_test(datasets, n_configs=25, smbo_n_init_configs=8, smbo_n_samples=128)
+    svr_test(datasets, n_configs=36, smbo_n_init_configs=8, smbo_n_samples=256)
     # random_forest_test(datasets, n_configs=243)
 
 
